802acdb78add7e40f0f8b6044233a840e1f01d3e
Mark Booth <mrbooth@gmail.com>
2016-07-08 15:20:12 +0100

    basic grid with jquery highlighting

--- application.js
+++ application.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
+  $(".container").append(new Array(257).join('<div class="bit"></div>'));
 
+  $(".bit").mouseenter(function(){
+    $(this).toggleClass("active");
+    if ($(this).css("border-color") == "rgb(128, 128, 128)") {
+      $(this).css("border-color", "black");
+    } else {
+      $(this).css("border-color", "rgb(128, 128, 128)");
+    }
+  });
+
+  $("button[name='clear_button']").click(function(){
+    $(".bit").removeClass("active").css("border-color", "rgb(128, 128, 128)");
+  });
+
+  $("button[name='reset_button']").click(function(){
+    var grid_size = prompt("How many squares in the new grid?", "16");
+    var new_grid = grid_size * grid_size
+    
+  });
 });
