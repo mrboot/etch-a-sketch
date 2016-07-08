f81115afff70bc50edcb4396f73db891bb2951db
Mark Booth <mrbooth@gmail.com>
2016-07-08 17:03:33 +0100

    now with flexible grid size

--- application.js
+++ application.js
@@ -1,22 +1,34 @@
 $(document).ready(function() {
-  $(".container").append(new Array(257).join('<div class="bit"></div>'));
-
-  $(".bit").mouseenter(function(){
-    $(this).toggleClass("active");
-    if ($(this).css("border-color") == "rgb(128, 128, 128)") {
-      $(this).css("border-color", "black");
-    } else {
-      $(this).css("border-color", "rgb(128, 128, 128)");
-    }
-  });
+  newGrid(64);
 
   $("button[name='clear_button']").click(function(){
     $(".bit").removeClass("active").css("border-color", "rgb(128, 128, 128)");
   });
 
   $("button[name='reset_button']").click(function(){
-    var grid_size = prompt("How many squares in the new grid?", "16");
-    var new_grid = grid_size * grid_size
-    
+    clearGrid();
+    var grid_size = prompt("How many squares wide is the new grid?", "64");
+    newGrid(grid_size);
   });
+
 });
+
+function newGrid(size) {
+  var container_size = $(".container").width();
+  var square_size = String((container_size / size) - 2) + "px";
+  var new_grid = (size * size) + 1;
+  $(".container").append(new Array(new_grid).join('<div class="bit"></div>'));
+  $(".bit").width(square_size);
+  $(".bit").height(square_size);
+
+  // need to have the mouseenter inside this function rather than inside
+  // $(document).ready for some reason?
+  $(".bit").mouseenter(function(){
+    $(this).addClass("active");
+  });
+
+};
+
+function clearGrid() {
+  $(".container").empty();
+};
