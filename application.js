57abd69e50d5c0f8d85a3da199f7d32a2c4de068
Mark Booth <mrbooth@gmail.com>
2016-07-10 11:42:00 +0100

    Add buttons for each type of grid and link them to different css classes

--- application.js
+++ application.js
@@ -1,33 +1,35 @@
 $(document).ready(function() {
-  newGrid(64);
+  newGrid(64, "black");
 
   $("button[name='clear_button']").click(function(){
-    $(".bit").removeClass("active").css("border-color", "rgb(128, 128, 128)");
+    $(".square").removeClass("black multicolour gradient").addClass("initial");
   });
 
   $("button[name='reset_button']").click(function(){
+    var type = $(this).attr("id")
     clearGrid();
     var grid_size = prompt("How many squares wide is the new grid?", "64");
-    newGrid(grid_size);
+    newGrid(grid_size, type);
   });
 
 });
 
-function newGrid(size) {
+function newGrid(size, type) {
   var container_size = $(".container").width();
   var square_size = String((container_size / size) - 2) + "px";
   var new_grid = (size * size) + 1;
-  $(".container").append(new Array(new_grid).join('<div class="bit"></div>'));
-  $(".bit").width(square_size);
-  $(".bit").height(square_size);
+  $(".container").append(new Array(new_grid).join('<div class="square initial"></div>'));
+  $(".square").width(square_size);
+  $(".square").height(square_size);
 
-  // need to have the mouseenter inside this function rather than inside
-  // $(document).ready for some reason?
-  $(".bit").mouseenter(function(){
-    $(this).addClass("active");
-  });
-
+  changeColours(type)
 };
+
+function changeColours(colour) {
+  $(".square").mouseenter(function(){
+    $(this).addClass(colour);
+  });
+}
 
 function clearGrid() {
   $(".container").empty();
