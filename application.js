e1bb2dfd0791c12183ed620dbc5bd372e9d541c0
Mark Booth <mrbooth@gmail.com>
2016-07-14 15:15:07 +0100

    wrangle random colour and gradient functions into the javascript

--- application.js
+++ application.js
@@ -1,20 +1,15 @@
 $(document).ready(function() {
-  newGrid(64, "black");
+  newGrid(64);
 
-  $("button[name='clear_button']").click(function(){
-    $(".square").removeClass("black multicolour gradient").addClass("initial");
-  });
-
-  $("button[name='reset_button']").click(function(){
-    var type = $(this).attr("id")
-    clearGrid();
-    var grid_size = prompt("How many squares wide is the new grid?", "64");
-    newGrid(grid_size, type);
+  $("button").click(function() {
+    var type = ($(this).prop("name"))
+    setStyle(type);
+    changeColour();
   });
 
 });
 
-function newGrid(size, type) {
+function newGrid(size) {
   var container_size = $(".container").width();
   var square_size = String((container_size / size) - 2) + "px";
   var new_grid = (size * size) + 1;
@@ -22,15 +17,50 @@ function newGrid(size, type) {
   $(".square").width(square_size);
   $(".square").height(square_size);
 
-  changeColours(type)
+  changeColour()
 };
 
-function changeColours(colour) {
-  $(".square").mouseenter(function(){
-    $(this).addClass(colour);
-  });
-}
+function randomColour() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+};
 
-function clearGrid() {
-  $(".container").empty();
+function resetGrid() {
+  $(".square").css({
+    "background-color": "rgb(255,255,255)",
+    "border": "1px solid gray"
+  });
+};
+
+function rgbDarker(rgb){
+ rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+ return "rgb(" +
+  ((parseInt(rgb[1],10)-8).toString()) + ", " +
+  ((parseInt(rgb[1],10)-8).toString()) + ", " +
+  ((parseInt(rgb[1],10)-8).toString()) + ")";
+};
+
+function changeColour() {
+  $(".square").on("mouseenter", function() {
+    if ($(this).hasClass("colour")) {
+      var randColour = randomColour();
+      var style = { backgroundColor: randColour, border: "1px solid "+randColour };
+    } else if ($(this).hasClass("gradient")) {
+      var darkerCol = rgbDarker($(this).css("background-color"));
+      var style = { backgroundColor: darkerCol, border: "1px solid " +darkerCol };
+    } else {
+      var style = { backgroundColor: "black", border: "1px solid black" };
+    }
+    $(this).css(style);
+  });
+};
+
+function setStyle(type) {
+  resetGrid();
+  if (type === "reset") {
+    $(".square").removeClass("colour gradient");
+  } else if (type === "colour") {
+    $(".square").removeClass("gradient").addClass("colour");
+  } else if (type === "gradient") {
+    $(".square").removeClass("colour").addClass("gradient");
+  }
 };
